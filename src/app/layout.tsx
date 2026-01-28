79e84f36fdf87fcbc754a3b3b034475d55ad41b5
Jerrod Mathis <jerrodmathis@proton.me>
2026-01-27 22:48:57 -0500

    fix: Update redirects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Sport Event Manager",
+  description: "The best way to manage your sports events.",
 };
 
 const geistSans = Geist({
--- src/components/auth-button.tsx
+++ src/components/auth-button.tsx
@@ -16,10 +16,10 @@ export async function AuthButton() {
   ) : (
     <div className="flex gap-2">
       <Button asChild size="sm" variant={"outline"}>
-        <Link href="/login">Sign in</Link>
+        <Link href="/auth/login">Sign in</Link>
       </Button>
       <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
+        <Link href="/auth/sign-up">Sign up</Link>
       </Button>
     </div>
   );
